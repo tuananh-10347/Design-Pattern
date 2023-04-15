04b9b3f3b1ea3b7a3d3dc15ec1bb886e38e25243
tuananh-10347 <traqnngoanhtuan@gmail.com>
2023-04-15 17:09:34 +0700

    Bridge Pattern

---
+++ Bridge/Bridge2/build/CMakeFiles/Bridge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bridge.dir/main.cpp.o"
+  "Bridge.pdb"
+  "Bridge"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
